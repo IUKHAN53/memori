934456ddb129f0172a98fb1250bb0b19bbeaacc6
Irfan Ullah Khan <iu.khan53@gmail.com>
2024-02-19 19:49:02 +0500

    Image cropper completed

--- app/Livewire/User/Profile.php
+++ app/Livewire/User/Profile.php
@@ -2,12 +2,48 @@
 
 namespace App\Livewire\User;
 
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class Profile extends Component
 {
+    protected $listeners = ['saveProfileImage'];
+
     public function render()
     {
         return view('livewire.user.profile');
     }
+
+    public $field;
+    public $profileImage;
+
+    public function mount()
+    {
+        $this->field = [
+            'name' => 'image',
+            'label' => 'Upload Image',
+            'key' => 'image',
+            'id' => 'imageCropper',
+            'width' => 250,
+            'height' => 250,
+            'shape' => 'circle',
+            'wrapperClass' => 'w-50',
+            'thumbnail' => '',
+            'disabled' => false,
+        ];
+    }
+
+
+    public function saveProfileImage()
+    {
+        $imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $this->profileImage));
+        $fileName = 'avatars/profile-image-' . time() . '.png';
+        Storage::disk('public')->put($fileName, $imageData, 'public');
+        $user = auth()->user();
+        $user->update([
+            'avatar' => $fileName
+        ]);
+
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -24,6 +25,8 @@ class User extends Authenticatable
         'country',
         'email',
         'password',
+        'role',
+        'avatar',
     ];
 
     /**
@@ -54,5 +57,10 @@ class User extends Authenticatable
     public function getNameAttribute()
     {
         return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getPictureAttribute()
+    {
+        return $this->avatar ? Storage::url($this->avatar) : 'https://ui-avatars.com/api/?name=' . urlencode($this->name) . '&color=7F9CF5&background=EBF4FF';
     }
 }
